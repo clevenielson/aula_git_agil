fe475fda02125773f7132eaea1dfa915e903a18c
clevenielson <clevenielson.vieira@gmail.com>
2024-05-18 17:06:44 -0300

    feat: Implementing light message

--- messages.py
+++ messages.py
@@ -2,5 +2,6 @@ display_messages = [
     'Seja Feliz :)',
     'Fique tranquilo, tudo vai acabar bem!',
     'ola mundo! estou aqui!',
-    'acredite nos seus caminhos! Sempre!'
+    'acredite nos seus caminhos! Sempre!',
+    'Sempre apague as luzes antes de sair!'
 ]
